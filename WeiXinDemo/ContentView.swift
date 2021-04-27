91724654d111d7b4c6e8e248e4c93d867a89ff35
Clare320 <lilingjie1992@gmail.com>
2021-04-27 22:24:49 +0800

    Initial Commit

---
+++ WeiXinDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeiXinDemo
+//
+//  Created by lingjie on 2021/4/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeiXinDemo/WeiXinDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeiXinDemoApp.swift
+//  WeiXinDemo
+//
+//  Created by lingjie on 2021/4/27.
+//
+
+import SwiftUI
+
+@main
+struct WeiXinDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
